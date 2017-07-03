a01e17f5e5c11c5d8074e038b60f2e6eebf49af1
wsy0240 <wsy0240@163.com>
2017-07-03 18:58:54 +0800

    HelloWorldApp
    Hello World！

---
+++ HelloWorldApp.java
@@ -0,0 +1,9 @@
+/**
+  * The HelloWorldApp class implements an application theat
+  * simply prints "Hello World!" to standard output
+  */
+ class HelloWorldApp{
+         public static void main(String[] args) {
+		         System.out.println("Hello World!");// Display the string.
+		 }
+ } 
